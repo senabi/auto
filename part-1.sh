dffbf814d07466cb36e1c5700afc27544aabffb7
senabi <sensenabi@gmail.com>
2021-03-30 00:09:28 -0500

    fix

--- part-1.sh
+++ part-1.sh
@@ -5,43 +5,44 @@
 ## lsblk
 # master boot record/ uefi
 dirpath=$(cd $(dirname $0); pwd -P)
+echo "$dirpath/variables.sh"
 . "$dirpath/variables.sh"
 
-[ $BIOS_TYPE = "bios" ] \
-  && parted /dev/$DISK1 mklabel msdos \
-  && echo "==> Label: MSDOS [done]" || echo "==> Label: MSDOS [failed]"
+[ $BIOS_TYPE = "bios" ] && \
+#  && parted /dev/$DISK1 mklabel msdos \
+  echo "==> Label: MSDOS [done]" || echo "==> Label: MSDOS [failed]"
   
-[ $BIOS_TYPE = "uefi" ] \
-  && parted /dev/$DISK1 mklabel gpt \
-  && echo "==> Label: GPT [done]" || echo "==> Label: GPT [failed]"
+[ $BIOS_TYPE = "uefi" ] && \
+  parted /dev/$DISK1 mklabel gpt && \
+  echo "==> Label: GPT [done]" || echo "==> Label: GPT [failed]"
 
 ## BOOT NO-SWAP
 # /boot = 1GiB
 # / = 100%
 PARTED="parted /dev/"
 
-[ $BIOS_TYPE = "bios" ] && [ $OPTION = "alt-1" ]\
-  && $PARTED$DISK1 mkpart primary $FS 0% 1GiB \
-  && $PARTED$DISK1 set 1 boot on \
-  && $PARTED$DISK1 mkpart primary $FS 1GiB 100% \
-  && echo "==> Partitions BIOS [done]" \
-  || echo "==> Partitions BIOS [failed]"
+[ $BIOS_TYPE = "bios" ] && [ $OPTION = "alt-1" ] && \
+  $PARTED$DISK1 mkpart primary $FS 0% 1GiB && \
+  $PARTED$DISK1 set 1 boot on && \
+  $PARTED$DISK1 mkpart primary $FS 1GiB 100% && \
+  echo "==> Partitions BIOS [done]" ||\
+  echo "==> Partitions BIOS [failed]"
 
-[ $BIOS_TYPE = "uefi" ] && [ $OPTION = "alt-1" ] \
-  && $PARTED$DISK1 set 1 esp on \
-  && $PARTED$DISK1 mkpart "EFI system partition" fat32 0% 261MiB \
-  && $PARTED$DISK1 mkpart "root partition" $FS 261MiB 100% \
-  && echo "==> Partitions UEFI [done]" \
-  || echo "==> Partitions BIOS [failed]"
+[ $BIOS_TYPE = "uefi" ] && [ $OPTION = "alt-1" ] && \
+  $PARTED$DISK1 set 1 esp on && \
+  $PARTED$DISK1 mkpart "EFI system partition" fat32 0% 261MiB && \
+  $PARTED$DISK1 mkpart "root partition" $FS 261MiB 100% && \
+  echo "==> Partitions UEFI [done]" || \
+  echo "==> Partitions BIOS [failed]"
 
 lsblk
 
-mount /dev/$DISK1 /mnt \
-  && mkdir /mnt/boot \
-  && mount /dev/$BOOT_PART /mnt/boot \
-  && echo "==> Mounted partitions [done]" \
-  && pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware \
-  || echo "==> Mounted partitions [failed]"
+mount /dev/$DISK1 /mnt && \
+  mkdir /mnt/boot && \
+  mount /dev/$BOOT_PART /mnt/boot && \
+  echo "==> Mounted partitions [done]" && \
+  pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware || \
+  echo "==> Mounted partitions [failed]"
 
 ## BOOT NO-SWAP
 # /boot = 1GiB
--- part-2.sh
+++ part-2.sh
@@ -5,24 +5,24 @@ dirpath=$(cd $(dirname $0); pwd -P)
 . "$dirpath/variables.sh"
 
 # Time zone
-ln -sf /usr/share/zoneinfo/US/Central /etc/localtime \
-  && echo "==> Time zone [done]"
+ln -sf /usr/share/zoneinfo/US/Central /etc/localtime && \
+  echo "==> Time zone [done]"
 
 # Localization
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen \
-  && echo "en_US ISO-8859-1" >> /etc/locale.gen \
-  && locale-gen \
-  && echo "LANG=en_US.UTF-8" > /etc/locale.conf \
-  && echo "KEYMAP=us" > /etc/vconsole.conf \
-  && echo "==> Localization [done]"
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
+  echo "en_US ISO-8859-1" >> /etc/locale.gen && \
+  locale-gen && \
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf && \
+  echo "KEYMAP=us" > /etc/vconsole.conf && \
+  echo "==> Localization [done]"
 
 # Network config
-echo "WICKED" > /etc/hostname \
-  && echo "==> Network config [done]"
+echo "WICKED" > /etc/hostname && \
+  echo "==> Network config [done]"
 
-pacman -S --noconfirm grub networkmanager \
-  && grub-install /dev/$DISK1 \
-  && grub-mkconfig -o /boot/grub/grub.cfg \
-  && echo "==> Bootloader GRUB [done]"
+pacman -S --noconfirm grub networkmanager && \
+  grub-install /dev/$DISK1 && \
+  grub-mkconfig -o /boot/grub/grub.cfg && \
+  echo "==> Bootloader GRUB [done]"
 
 passwd
